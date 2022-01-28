bdafdd4e6be0ce8cd4050ccd9c3e80335ede13fc
wangtongyou <wangtongyou@zhiwang.com>
2022-01-28 16:36:53 +0800

    koa-body解析请求数据

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,19 @@
 const Koa = require("koa");
 const Router = require('koa-router');
+const KoaBody = require("koa-body");
 
 const userRouter = require('../router/user.route');
 
 
 const app = new Koa();
-const indexRouter = new Router();
 
-indexRouter.get('/', (ctx, next)=> {
-  ctx.body = 'Hello World';
-});
+// const indexRouter = new Router();
+// indexRouter.get('/', (ctx, next)=> {
+//   ctx.body = 'Hello World';
+// });
+// app.use(indexRouter.routes()).use(indexRouter.allowedMethods());
 
-
-app.use(indexRouter.routes()).use(indexRouter.allowedMethods());
+app.use(KoaBody());
 app.use(userRouter.routes());
 
 module.exports = app;
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,9 +1,17 @@
+const { createUser } = require("../service/user.service");
+
 class USerController {
   async reqister(ctx, next) {
-    ctx.body = '用户注册';
+    const {user_name,password} = ctx.request.body;
+    // 操作数据库
+    const res = await createUser(user_name,password);
+    // 返回结果
+    ctx.body =res;
+
   }
 
   async login(ctx, next) {
+    
     ctx.body = '用户登录';
   }
 }
